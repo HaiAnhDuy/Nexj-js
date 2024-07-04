c324991ef3fd19e0a9c594e1fb45360f9770d7de
Hai Anh Duy <haianhduywork@gmail.com>
2024-07-04 16:12:11 +0700

    update 3

--- src/components/users/user.table.tsx
+++ src/components/users/user.table.tsx
@@ -1,45 +1,108 @@
-import '../../style/userpage.css'
+import { Interface } from 'readline'
+// import '../../style/userpage.scss';
 
+import { Space, Table, Tag } from 'antd';
+import type { TableProps } from 'antd';
+
+
+import { useEffect, useState } from 'react'
 const UserTable = () => {
+    const [DataTest1, SetData1] = useState([])
+    interface Iitems {
+        _id: string,
+        address: string,
+        age: number,
+        email: string
+    }
+
+    const GetData1 = async () => {
+        const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+            method: 'POST',
+            body: JSON.stringify({
+                username: 'admin@gmail.com',
+                password: '123456'
+            },
+            ),
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        });
+
+        const data = await res.json();
+        // console.log(data.data.access_token)
+        if (data && data.data && data.data.access_token) {
+            const res1 = await fetch("http://localhost:8000/api/v1/users/all", {
+
+                headers: {
+                    'Authorization': `Bearer ${data.data.access_token}`,
+                    "Content-Type": "application/json"
+                }
+
+            });
+            console.log(data.data.access_token)
+            const data1 = await res1.json();
+
+            console.log('>>> check', data1.data.result)
+            if (data1 && data1.data && data1.data.result && data1.data.result.length > 0) {
+                SetData1(data1.data.result)
+            }
+
+        }
+
+    }
+
+    useEffect(() => {
+        // update
+        GetData1();
+    }, [])
+
+
+    const columns: TableProps<Iitems>['columns'] = [
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+
+
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Invite {record.email}</a>
+                    <a>Delete</a>
+                </Space>
+            ),
+        },
+    ];
+    const TableUser = () => {
+        return (
+            <Table
+                columns={columns}
+                dataSource={DataTest1}
+                rowKey={'_id'}
+            />
+
+        )
+    }
+
     return (
+
         <div>
-            <table>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
-            </table>
+            <TableUser />
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
+  Link,
+  useLocation
 } from "react-router-dom";
 import UserPage from './screens/user.page.tsx';
+import { useState } from 'react'
+import { AppstoreOutlined, MailOutlined, SettingOutlined, HomeOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+
+
 // import './index.css'
 // const router = createBrowserRouter([
 //   {
@@ -13,15 +22,61 @@ import UserPage from './screens/user.page.tsx';
 //     element: <div>Hello world!</div>,
 //   },
 // ]);
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to={'/'}>Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to={'/hello'}>Table User</Link>,
+    key: 'user',
+    icon: <AppstoreOutlined />,
+
+  },
+
+
+];
+const Header = () => {
+  const location = useLocation();
+
+
+  const [current, setCurrent] = useState(location.pathname === '/' ? 'home' : 'user');
+
+
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+const LayOut = () => {
+  const location = useLocation();
+  console.log('location', location)
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <footer>FOOTER</footer>
+    </div>
+  )
+}
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LayOut />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: '/hello',
+        element: <UserPage />
+      }
+    ]
   },
-  {
-    path: '/hello',
-    element: <UserPage />
-  }
+
 ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
